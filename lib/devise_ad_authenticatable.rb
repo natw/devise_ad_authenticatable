97b5fd1f2cd949516882a263cf5e0db3ff8accca
Nat Williams <nwilliams@leapfrogonline.com>
2013-09-22 17:32:00 -0500

    add setters for config options on Devise module

--- lib/devise_ad_authenticatable.rb
+++ lib/devise_ad_authenticatable.rb
@@ -4,6 +4,20 @@ require 'devise_ad_authenticatable/strategy'
 require 'devise_ad_authenticatable/model'
 
 
+module Devise
+  [:ad_host, :ad_port, :ad_search_root, :ad_domain].map(&:to_s).each do |attr|
+    class_eval %Q"
+      def self.#{attr}
+        @@#{attr}
+      end
+      def self.#{attr}=(a)
+        @@#{attr} = a
+      end
+    "
+  end
+  @@ad_port = '389'
+end
+
 Devise.add_module(
   :ad_authenticatable,
   :route => :session,
